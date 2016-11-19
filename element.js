1ce1665a5808f5ed8699d74ba933dd1b09f07fc6
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-11-19 16:26:17 -0700

    moving code

--- element.js
+++ element.js
@@ -2,14 +2,6 @@
 import preact from 'preact'
 import ToggleComponent from './components'
 
-// https://github.com/w3c/webcomponents/issues/587#issuecomment-254126763
-function BabelHTMLElement() {
-  const newTarget = this.__proto__.constructor
-  return Reflect.construct(HTMLElement, [], newTarget)
-}
-Object.setPrototypeOf(BabelHTMLElement, HTMLElement)
-Object.setPrototypeOf(BabelHTMLElement.prototype, HTMLElement.prototype)
-
 export default class ToggleElement extends BabelHTMLElement {
   attributeChangedCallback(name, oldValue, newValue) {
     if (this.rendered) { this.updateRendering() }
@@ -26,3 +18,11 @@ export default class ToggleElement extends BabelHTMLElement {
     this.rendered = true
   }
 }
+
+// https://github.com/w3c/webcomponents/issues/587#issuecomment-254126763
+function BabelHTMLElement() {
+  const newTarget = this.__proto__.constructor
+  return Reflect.construct(HTMLElement, [], newTarget)
+}
+Object.setPrototypeOf(BabelHTMLElement, HTMLElement)
+Object.setPrototypeOf(BabelHTMLElement.prototype, HTMLElement.prototype)
