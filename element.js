7be7e96b07ed0cf97f93e39b43d80784d5d6e697
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-11-23 15:54:53 -0700

    use create-element-class module

--- element.js
+++ element.js
@@ -1,21 +1,21 @@
 /** @jsx preact.h */
 import preact from 'preact'
 import ToggleComponent from './components'
-import HTMLElement from 'babel-html-element'
+import createElementClass from 'create-element-class'
 
-export default class ToggleElement extends HTMLElement {
+export default createElementClass({
   attributeChangedCallback(name, oldValue, newValue) {
     if (this.rendered) { this.updateRendering() }
-  }
+  },
 
   connectedCallback() {
     this.input = this.querySelector('input')
     this.input.className += 'shaf-toggle-screenreader-only'
     this.updateRendering()
-  }
+  },
 
   updateRendering() {
-    const a = preact.render(<ToggleComponent input={this.input} />, this, this.lastChild)
+    preact.render(<ToggleComponent input={this.input} />, this, this.lastChild)
     this.rendered = true
   }
-}
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
